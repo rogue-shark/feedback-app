0b768e2f1656c675ac6a9560deeb26d561457c48
RogueShark <shantanuss789@gmail.com>
2023-01-21 19:49:39 +0530

    Added animation: For: Add/remove feedback
    using NPM > framer-motion@4.1.17
    
    - Added fade-in/out effect for each feedback item in feedback-list when added or removed

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,21 +1,44 @@
 import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return 'No feedback data entries.';
   }
+  //with framer-motion
   return (
     <div className='feedback-list'>
-      {feedback.map((item) => (
-        <FeedbackItem
-          key={item.id}
-          item={item}
-          handleDelete={handleDelete} //passing upwards to App.js
-        />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.dev
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete} //passing upwards to App.js
+            />
+          </motion.dev>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem
+  //         key={item.id}
+  //         item={item}
+  //         handleDelete={handleDelete} //passing upwards to App.js
+  //       />
+  //     ))}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {
